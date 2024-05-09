514e771ea5b1e1d444f1531314065ae39690275e
Ville Vornanen <8843354+vvornanen@users.noreply.github.com>
2024-05-09 08:40:17 +0300

    Truncate main story abstract

--- src/components/MainStory/MainStory.js
+++ src/components/MainStory/MainStory.js
@@ -1,14 +1,8 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
+import { QUERIES } from "../../constants";
 
-const MainStory = ({
-  id,
-  title,
-  image,
-  location,
-  abstract,
-  ...delegated
-}) => {
+const MainStory = ({ id, title, image, location, abstract, ...delegated }) => {
   return (
     <Wrapper {...delegated}>
       <a href={`/story/${id}`}>
@@ -44,6 +38,14 @@ const Abstract = styled.p`
   font-size: 1rem;
   margin-bottom: 1em;
   white-space: pre-wrap;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 8;
+  overflow: hidden;
+
+  @media ${QUERIES.tabletOnly} {
+    -webkit-line-clamp: 15;
+  }
 `;
 
 const Location = styled.span`
