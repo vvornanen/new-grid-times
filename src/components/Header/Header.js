811758c1deb0c6c5f11ba1d67e67433b5f87e710
Ville Vornanen <8843354+vvornanen@users.noreply.github.com>
2024-05-09 08:19:48 +0300

    Add desktop header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components/macro";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { QUERIES } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
   return (
@@ -29,7 +29,21 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <DesktopNav>
+          <ActionGroup>
+            <button>
+              <Search size={24} />
+            </button>
+            <button>
+              <Menu size={24} />
+            </button>
+          </ActionGroup>
+        </DesktopNav>
         <Logo />
+        <DesktopUserActions>
+          <Button>Subscribe</Button>
+          <TextLink href="/login">Already a subscriber?</TextLink>
+        </DesktopUserActions>
       </MainHeader>
     </header>
   );
@@ -39,6 +53,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -65,6 +83,44 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    margin-block-start: 48px;
+    margin-block-end: 72px;
+  }
+
+  @media ${QUERIES.desktopAndUp} {
+    justify-content: space-between;
+    margin-block-start: 16px;
+    margin-block-end: 72px;
+  }
+`;
+
+const DesktopNav = styled.nav`
+  display: none;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: revert;
+  }
+`;
+
+const DesktopUserActions = styled.div`
+  display: none;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: flex;
+    flex-direction: column;
+    align-self: flex-end;
+    align-items: center;
+    gap: 8px;
+  }
+`;
+
+const TextLink = styled.a`
+  font-family: var(--font-family-serif);
+  font-size: ${14 / 16}rem;
+  font-style: italic;
+  color: var(--color-gray-900);
 `;
 
 export default Header;
